66786e29e28fd7b2de4c5a3e08eb6a68cb3ae178
渡边 <1397556989@qq.com>
2023-06-19 22:43:14 +0800

    fix: 解决了一个Spotbugs提示的bug：生成的随机数质量较差 调整了一下部分代码格式

--- src/main/java/cn/edu/whut/sept/zuul/GoCommand.java
+++ src/main/java/cn/edu/whut/sept/zuul/GoCommand.java
@@ -6,17 +6,15 @@
  * @version 1.0
  * */
 
-
 package cn.edu.whut.sept.zuul;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
-public class GoCommand extends Command
-{
+public class GoCommand extends Command {
     /**
-     * 执行“前进”命令
+     * 执行“前进”命令。
      *
      * @param game 游戏对象
      * @return 如果命令执行完毕返回true，否则返回false
@@ -24,7 +22,7 @@ public class GoCommand extends Command
     @Override
     public boolean execute(Game game)
     {
-        if(!hasSecondWord()) {
+        if (!hasSecondWord()) {
             System.out.println("去哪?");
         }
 
@@ -38,12 +36,12 @@ public class GoCommand extends Command
             System.out.println("这个方向没有房间!");
         }
         else {
-            if(game.getPlayer()!=null)
+            if (game.getPlayer() != null)
             {
-                HashMap<String, Monster> monsters=currentRoom.getMonsters();
-                if(monsters.get(direction)!=null){
-                    boolean flag=game.getPlayer().fight(monsters.get(direction));
-                    if(!flag){
+                HashMap<String, Monster> monsters = currentRoom.getMonsters();
+                if (monsters.get(direction) != null){
+                    boolean flag = game.getPlayer().fight(monsters.get(direction));
+                    if (!flag){
                         game.setFail();
                         System.out.println("你被怪物击败，濒临死亡！");
                         return true;
@@ -51,7 +49,7 @@ public class GoCommand extends Command
                     System.out.println("你获得了胜利！");
                     System.out.println();
                 }
-                if(nextRoom.getDescription().equals("在一个有传送门的房间！")){
+                if (nextRoom.getDescription().equals("在一个有传送门的房间！")){
                     game.setWin();
                     System.out.println("你找到了传送门，逃离了这个混乱的区域");
                     return true;
@@ -62,7 +60,7 @@ public class GoCommand extends Command
                     // 随机选择目标房间
                     ArrayList<Room> teleportRooms = game.getRooms();
                     Random random = new Random();
-                    int randomIndex = random.nextInt(teleportRooms.size());
+                    int randomIndex = getRandomIndex(random, teleportRooms.size());
                     nextRoom = teleportRooms.get(randomIndex);
                 }
                 game.getPlayer().enterRoom(nextRoom);
@@ -75,5 +73,9 @@ public class GoCommand extends Command
         }
 
         return false;
+    }
+
+    private int getRandomIndex(Random random, int size) {
+        return random.nextInt(size);
     }
 }
